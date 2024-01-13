4ab4e6be59dc15ab58b16b81e3ccc210786a201e
SalleeMatthew <132789789+SalleeMatthew@users.noreply.github.com>
2024-01-13 11:15:05 -0600

    Update message.routes.ts

--- api/src/routes/message.routes.ts
+++ api/src/routes/message.routes.ts
@@ -7,6 +7,7 @@ messageRoutes.get('/place/:placeId',
   (request, response) => messageController.getResults(request, response));
 messageRoutes.post('/place/:placeId',
   (request, response) => messageController.addMessage(request, response));
+messageRoutes.post('/deletemessage/',
+  (request, response) => messageController.deleteMessage(request, response));
 
 export { messageRoutes };
-
